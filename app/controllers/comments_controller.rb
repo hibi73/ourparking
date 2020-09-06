cd82d6c5bd01f764b85b23bcaf2f4f69d0e32772
hibi73 <66819398+hibi73@users.noreply.github.com>
2020-09-06 20:19:56 +0900

    createアクションをコントローラーに定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+  def create
+    @park = Item.find(params[:park_id])
+    Comment.create(comment_params)
+    redirect_to item_path(@park.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, park_id: params[:park_id])
+  end
+
 end
